36f19cbf847941a4163085655b814eab849a6b5d
Débora Rocha <debora.rocha@iatec.com>
2022-11-07 16:32:52 -0300

    Exemplo 4

--- main.js
+++ main.js
@@ -5,17 +5,38 @@
 //  document.querySelector('.tecla_pom');
 
 
-// EXEMPLO 2 
-  function tocaSomPom () {
-     document.querySelector('#som_tecla_pom').play();
- }
-//  document.querySelector('.tecla_pom').onClick = tocaSomPom; 
+// EXEMPLO 2
+//   function tocaSomPom () {
+//      document.querySelector('#som_tecla_pom').play();
+//  }
+//  document.querySelector('.tecla_pom').onClick = tocaSomPom;
 
 
-// EXEMPLO 3 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onCLick = tocaSomPom;
-listaDeTeclas[1].onCLick = tocaSomClap;
-listaDeTeclas[2].onCLick = tocaSomTim;
-listaDeTeclas[3].onCLick = tocaSomPush;
-listaDeTeclas[4].onCLick = tocaSomSplash;
+// EXEMPLO 3
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+// listaDeTeclas[0].onCLick = tocaSomPom;
+// listaDeTeclas[1].onCLick = tocaSomClap;
+// listaDeTeclas[2].onCLick = tocaSomTim;
+// listaDeTeclas[3].onCLick = tocaSomPush;
+// listaDeTeclas[4].onCLick = tocaSomSplash;
+
+
+// EXEMPLO 4
+function tocaSom(idElementAudio) {
+  document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+  listaDeTeclas[contador].onCLick = function () {
+    tocaSom('#som_tecla_pom');
+  }
+
+  contador = contador + 1;
+
+  console.log(tocaSom);
+}
