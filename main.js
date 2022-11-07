3ed20d322cc87ebf5c29492ce8811393ca0b9e89
Débora Rocha <debora.rocha@iatec.com>
2022-11-07 15:25:42 -0300

    Como pegar o som e associar ao botão
    Exemplo 1 e 2

---
+++ main.js
@@ -0,0 +1,14 @@
+// EXEMPLO 1
+/* function tocaSomPom () {
+     document.querySelector('#som_tecla_pom').play();
+ }
+ document.querySelector('.tecla_pom');*/
+
+
+// EXEMPLO 2 
+/*  function tocaSomClap () {
+     document.querySelector('#som_tecla_clap').play();
+ }
+ document.querySelector('.tecla_pom').onClick = tocaSomPom; */
+
+
