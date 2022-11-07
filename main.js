ec59f882be510c5a9caf69d44ee58dde03bc1376
Débora Rocha <debora.rocha@iatec.com>
2022-11-07 15:40:37 -0300

    Exemplo com Lista

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 // EXEMPLO 1
-/* function tocaSomPom () {
-     document.querySelector('#som_tecla_pom').play();
- }
- document.querySelector('.tecla_pom');*/
+//  function tocaSomPom () {
+//      document.querySelector('#som_tecla_pom').play();
+//  }
+//  document.querySelector('.tecla_pom');
 
 
 // EXEMPLO 2 
-/*  function tocaSomClap () {
-     document.querySelector('#som_tecla_clap').play();
+  function tocaSomPom () {
+     document.querySelector('#som_tecla_pom').play();
  }
- document.querySelector('.tecla_pom').onClick = tocaSomPom; */
+//  document.querySelector('.tecla_pom').onClick = tocaSomPom; 
 
 
+// EXEMPLO 3 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onCLick = tocaSomPom;
+listaDeTeclas[1].onCLick = tocaSomClap;
+listaDeTeclas[2].onCLick = tocaSomTim;
+listaDeTeclas[3].onCLick = tocaSomPush;
+listaDeTeclas[4].onCLick = tocaSomSplash;
