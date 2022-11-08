aab634c99946d6821a505bc3b589f5c49b01741c
Débora Rocha <debora.rocha@iatec.com>
2022-11-08 15:44:09 -0300

    Exemplo 6 - FOR

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// EXEMPLO 6
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`; //template string
+
+  tecla.onclick = function () {
+
+    tocaSom(idAudio);
+
+  };
+
+}
 
 // EXEMPLO 1
 //  function tocaSomPom () {
