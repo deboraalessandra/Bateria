a00d9a661e9ab4278c88cf433a6aeaa75465e881
Débora Rocha <debora.rocha@iatec.com>
2022-11-08 15:43:37 -0300

    Exemplo 5

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
+
 // EXEMPLO 1
 //  function tocaSomPom () {
 //      document.querySelector('#som_tecla_pom').play();
 //  }
 //  document.querySelector('.tecla_pom');
 
-
 // EXEMPLO 2
 //   function tocaSomPom () {
 //      document.querySelector('#som_tecla_pom').play();
 //  }
 //  document.querySelector('.tecla_pom').onClick = tocaSomPom;
-
 
 // EXEMPLO 3
 // const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -20,23 +19,51 @@
 // listaDeTeclas[3].onCLick = tocaSomPush;
 // listaDeTeclas[4].onCLick = tocaSomSplash;
 
-
 // EXEMPLO 4
-function tocaSom(idElementAudio) {
-  document.querySelector(idElementAudio).play();
-}
+// function tocaSom(idElementAudio) {
+//   document.querySelector(idElementAudio).play();
+// }
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
+// const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+// let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+// while (contador < listaDeTeclas.length) {
 
-  listaDeTeclas[contador].onCLick = function () {
-    tocaSom('#som_tecla_pom');
-  }
+//   listaDeTeclas[contador].onCLick = function () {
+//     tocaSom('#som_tecla_pom');
+//   }
 
-  contador = contador + 1;
+//   contador = contador + 1;
 
-  console.log(tocaSom);
-}
+//   console.log(tocaSom);
+// }
+
+// EXEMPLO 5
+// function tocaSom(idElementAudio) {
+//   document.querySelector(idElementAudio).play();
+// }
+
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// let contador = 0;
+
+// while (contador < listaDeTeclas.length) {
+
+//   const tecla = listaDeTeclas[contador];
+
+//   const instrumento = tecla.classList[1];
+
+//   // template string
+//   const idAudio = `#som_${instrumento}`;
+
+//   console.log(idAudio);
+
+//   tecla.onCLick = function () {
+//     tocaSom(idAudio);
+//   }
+
+//   contador = contador + 1;
+
+//   console.log(tocaSom);
+// }
